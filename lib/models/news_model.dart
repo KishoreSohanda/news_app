68be2325cc46a24af48b9de03548ad4401d31b0f
Kishore Sohanda <kishoresohanda100@gmail.com>
2024-04-08 15:52:04 +0530

    kotlin version upgraded and all errors are resolved now.

--- lib/models/news_model.dart
+++ lib/models/news_model.dart
@@ -1,10 +1,10 @@
 import 'package:http/http.dart' as http;
 
-import '../api_keys.dart';
+import '../api_keys.dart' as key;
 
 class NewsModel {
-  var url =
-      Uri.parse('https://newsapi.org/v2/top-headlines/sources?apiKey=$apiKey');
+  var url = Uri.parse(
+      'https://newsapi.org/v2/top-headlines/sources?apiKey=${key.apiKey}');
   Future<void> getNews() async {
     var res = await http.get(url);
     print(res.body);
